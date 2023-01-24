81ca7ee52b6f869d86a03a34b15d77acd537d03e
rasool <rsool_2001@yahoo.com>
2023-01-24 14:27:02 -0600

    Tests :sunglasses:

---
+++ frontend/capstone-ui/__tests__/home/home.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Home } from "../../src/home/home";
+import { render } from '@testing-library/react';
+
+it("renders home", () => {
+    const home = render(
+        <BrowserRouter>
+            <Home />
+        </BrowserRouter>
+    );
+
+    // Children existence.
+    expect(home.container.childNodes.length).toBeGreaterThan(0);
+});
