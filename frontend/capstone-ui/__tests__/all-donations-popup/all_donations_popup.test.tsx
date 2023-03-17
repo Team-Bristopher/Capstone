dfb7197acd5e4a3722f29ae390a4ed1fa42abf11
rasool <rsool_2001@yahoo.com>
2023-03-17 18:05:40 -0500

    Fixed build

--- frontend/capstone-ui/__tests__/all-donations-popup/all_donations_popup.test.tsx
+++ frontend/capstone-ui/__tests__/all-donations-popup/all_donations_popup.test.tsx
@@ -1,5 +1,5 @@
 import { faker } from "@faker-js/faker";
-import { act, render } from "@testing-library/react";
+import { act, render, RenderResult } from "@testing-library/react";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
@@ -7,6 +7,8 @@ import { AllDonationsPopup } from "../../src/all-donations-popup/all_donations_p
 import * as api from "../../src/api/api-calls";
 import { Home } from "../../src/home/home";
 import { FundraiserDonationMessage } from "../../src/models/incoming/FundraiserDonationMessage";
+
+jest.mock("../../src/api/api-calls");
 
 const getMockDonationMessage = (amt: number): Array<FundraiserDonationMessage> => {
    let donations: Array<FundraiserDonationMessage> = [];
@@ -25,6 +27,8 @@ const getMockDonationMessage = (amt: number): Array<FundraiserDonationMessage> =
 }
 
 describe("all donations popup tests", () => {
+   beforeEach(() => jest.clearAllMocks());
+
    it("renders all donations popup", () => {
       const queryClient = new QueryClient();
 
@@ -49,16 +53,27 @@ describe("all donations popup tests", () => {
       // properly being mocked.
       expect(jest.isMockFunction(getAllDonationsMock)).toBeTruthy();
 
-      getAllDonationsMock.mockResolvedValueOnce(getMockDonationMessage(100));
+      const mockDonationMessages = getMockDonationMessage(25);
 
-      let component: any;
+      getAllDonationsMock.mockResolvedValueOnce(mockDonationMessages);
+
+      let component: RenderResult;
 
       await act(async () => {
          component = render(
             <BrowserRouter>
-               <AllDonationsPopup onClose={() => { }} />
+               <QueryClientProvider client={queryClient}>
+                  <AllDonationsPopup onClose={() => { }} />
+               </QueryClientProvider>
             </BrowserRouter>
          );
+      });
+
+      // Expect to find all donation messages
+      // in the component listed. Only up to 25 since 
+      // we allow a max page of 25 results.
+      mockDonationMessages.forEach((message) => {
+         expect(component.findByText(message.message)).toBeTruthy()
       });
    });
 });
